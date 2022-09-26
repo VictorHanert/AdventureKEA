b1875b2227382576b24b883f1f8e0dd0102b6b67
VictorHanert <vict660m@stud.kea.dk>
2022-09-26 19:17:26 +0200

    Added "look" and "help" to switch-case

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 public class UserInterface {
     private final Scanner scan = new Scanner(System.in).useLocale(Locale.ENGLISH);
 
-
     public void start() {
         welcome();
+        direction();
     }
 
     public void welcome() {
-        System.out.println("Velkommen til KEA!");
-        direction();
+        System.out.println("Weclome to \u001b[1mKEA\u001b[0m!\nCan you finde the way to the canteen?");
     }
 
     public void direction() {
         String input = "";
 
         while (!input.equals("Exit")) {
-            input = scan.nextLine();
+            input = scan.nextLine().toLowerCase();
             switch (input) {
-                case "North" -> System.out.println("Going North");
-                case "South" -> System.out.println("Going south");
-                case "East" -> System.out.println("Going east");
-                case "West" -> System.out.println("Going west");
-                case "Exit" -> {
+                case "north" -> System.out.println("Going north");
+                case "south" -> System.out.println("Going south");
+                case "east" -> System.out.println("Going east");
+                case "west" -> System.out.println("Going west");
+                case "look" -> System.out.println("Getting the room description and location");
+                case "help" -> System.out.println("Get commands and instruction");
+                case "exit" -> {
+                    System.out.println("Shutting down.");
                     System.exit(1);
                 }
                 default -> System.out.println("Error");
