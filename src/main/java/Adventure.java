8af8221a9b9be1c36835e3ea2c60fbccb5de2195
VictorHanert <vict660m@stud.kea.dk>
2022-09-26 19:40:31 +0200

    Added room 2 and 3

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -4,6 +4,10 @@ public class Adventure {
     public void createRooms() {
         Room room1 = new Room("a classroom", "There is tables and chairs");
 
+        Room room2 = new Room("the toilet", "There is toilets and a hand wash");
+
+        Room room3 = new Room("the game-room", "There is table tennis");
+
         currentRoom = room1;
     }
 
