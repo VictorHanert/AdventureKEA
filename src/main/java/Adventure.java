dfe688c1700b28dc135c37ac7425495e6d29e41d
nicolaiandersson <nico602d@stud.kea.dk>
2022-09-27 16:30:43 +0200

    updated room text again

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -3,15 +3,15 @@ public class Adventure {
 
 
     public void createRooms() {
-        Room room1 = new Room("a classroom", "You enter a room. It looks like a classroom.");
+        Room room1 = new Room("a classroom", "In this room you see tables and chairs. There is also a projector and a whiteboard.");
         Room room2 = new Room("the toilet", "It smells bad in here. There are mirrors and a sink.");
-        Room room3 = new Room("the game-room", "You enter a room. The room has a tennis table and a small kitchen.");
-        Room room4 = new Room("the bicycle-basement", "You enter a big dark basement with dim lights. There are a lot of bicycles parked.");
-        Room room5 = new Room("the 'fredags-bar'", "You have arrived at the party. There are cheap 'buca-shots' and horny girls.");
-        Room room6 = new Room("the library", "You enter a big room with a lot of bookshelves. It looks like a library.");
-        Room room7 = new Room("the canteen", "You enter a big room with an overpriced shop and plenty of tables and chairs. It looks like a canteen.");
-        Room room8 = new Room("the office", "You enter a room. You see a lot of older people and they are all drinking coffee.");
-        Room room9 = new Room("the code-lab", "You enter a room. The room smells like ass, and there are zero bitches. There are people programming.");
+        Room room3 = new Room("the game-room", "The room has a tennis table and a small kitchen.");
+        Room room4 = new Room("the bicycle-basement", "You are in a big dark basement with dim lights. There are a lot of bicycles parked.");
+        Room room5 = new Room("the 'fredags-bar'", "You have arrived at the party. There are cheap 'buca-shots' and hot girls.");
+        Room room6 = new Room("the library", "The room you are in has a lot of bookshelves.");
+        Room room7 = new Room("the canteen", "This big room has an overpriced shop and plenty of tables and chairs.");
+        Room room8 = new Room("the office", "In this room you see a lot of older people and they are all drinking coffee.");
+        Room room9 = new Room("the code-lab", "Before entering this room you saw a sign on the window that said 'Code Lab'. There are people programming.");
 
         // Setting directions for each room
         room1.setEast(room2);
